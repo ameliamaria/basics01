8d10b12a27c5164d20a4a40dbb27d76d85c2064a
ameliamaria <ameliamaria.maciorowska@gmail.com>
2020-04-02 17:55:34 +0200

    pierwszy commit

---
+++ script01.py
@@ -0,0 +1,14 @@
+arg1 = 2
+arg2 = 1
+arg3 = False
+
+
+def funkcja(arg1, arg2, arg3):
+    if arg3 == True:
+        print (arg1 + arg2)
+
+    else:
+        print (arg1 - arg2)
+
+
+funkcja(arg1, arg2, arg3)
